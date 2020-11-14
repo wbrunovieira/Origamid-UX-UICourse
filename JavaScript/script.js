d6a91b7266467c723d9433c60eba7aecb7d87276
wbrunovieira <wbrunovieira77@gmail.com>
2020-11-14 07:40:21 +0000

    added wordpress gutenberg old feature, theme bikcraft

---
+++ JavaScript/script.js
@@ -0,0 +1,110 @@
+var nome = 'André';
+let idade = 28;
+const possuiFaculdade = true;
+
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+
+var nome = 'André',
+    idade = 28,
+    possuiFaculdade = true;
+
+    
+    // Declarar uma variável com o seu nome
+
+    const nome = 'Bruno'
+
+
+// Declarar uma variável com a sua idade
+
+let idade = 46
+
+// Declarar uma variável com a sua comida
+
+ 
+// favorita e não atribuir valor
+let comidaFavorita
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = 'pasta'
+
+// Declarar 5 variáveis diferentes sem valores
+
+let cor = 'Vermelho'
+let ano = 2020
+let isALanguage = true
+let mes = 'fevereiro'
+dia = 14
+
+var nome = 'André'; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol() // Symbol
+var novoObjeto = {} // Object
+
+
+var nome = 'André';
+console.log(typeof nome);
+
+
+// retorna string
+
+var nome = 'Bruno'
+
+// Declare uma variável contendo uma string
+
+
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+
+
+// Crie uma função para verificar se um valor é Truthy
+
+function SeriaVerdadeiro(number){
+    if(number===0) {console.log('verdadeiro')}
+} console.log('falso')
+
+SeriaVerdadeiro(10)
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+// Crie uma função que verifica se um número é par
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+
+// Corrija o erro abaixo
+function precisoVisitar(paisesVisitados) {
+    var totalPaises = 193;
+    return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+  }
+  function jaVisitei(paisesVisitados) {
+    return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+  }
+  precisoVisitar(20);
+  jaVisitei(20);
+  
